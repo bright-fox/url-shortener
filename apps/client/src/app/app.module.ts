a5042f69d389b0a2293e68f54aad10b7c95420ac
Hung Vu <leertet96@gmail.com>
2023-05-23 00:21:30 +0200

    Add url shortener form

--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -3,9 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ShortenUrlFormComponent } from './components/shortenUrlForm/shorten-url-form.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    HeaderComponent,
+    ShortenUrlFormComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ apps/client/src/app/components/shortenUrlForm/shorten-url-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'url-shortener-shorten-url-form',
+  templateUrl: './shorten-url-form.component.html',
+  styleUrls: ['./shorten-url-form.component.css'],
+})
+export class ShortenUrlFormComponent {}
