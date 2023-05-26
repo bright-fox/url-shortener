12140d27814b00cc9cd4f7a64006f9fa80741b59
Hung Vu <leertet96@gmail.com>
2023-05-26 13:17:44 +0200

    app(server): Setup url service test

--- apps/server/src/app/url/url.service.spec.ts
+++ apps/server/src/app/url/url.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UrlService } from './url.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Url } from './entities/url.entity';
+
+const urlMockRepo = {
+  findOne: jest.fn(),
+  create: jest.fn(),
+  save: jest.fn(),
+};
 
 describe('UrlService', () => {
   let service: UrlService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UrlService],
+      providers: [
+        UrlService,
+        { provide: getRepositoryToken(Url), useValue: urlMockRepo },
+      ],
     }).compile();
 
     service = module.get<UrlService>(UrlService);
