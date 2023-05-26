7a71e2899653ade92e7580991f4c126a54bb4890
Hung Vu <leertet96@gmail.com>
2023-05-26 12:39:52 +0200

    Create init structure for header unit tests

--- apps/client/src/app/components/header/header.component.spec.ts
+++ apps/client/src/app/components/header/header.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -7,6 +8,7 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [HeaderComponent],
     }).compileComponents();
 
