c78257dad0e1788cab5635c3b6e7e80e23d8a324
Hung Vu <leertet96@gmail.com>
2023-05-26 12:38:42 +0200

    Add init structure for app component unit tests

--- apps/client/src/app/app.component.spec.ts
+++ apps/client/src/app/app.component.spec.ts
@@ -1,27 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { UrlShortenerComponent } from './pages/urlShortener/url-shortener.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [],
-      declarations: [AppComponent, NxWelcomeComponent],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent, HeaderComponent, UrlShortenerComponent],
     }).compileComponents();
   });
 
-  it('should render title', () => {
+  it('should create app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome client'
-    );
-  });
-
-  it(`should have as title 'client'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('client');
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
   });
 });
